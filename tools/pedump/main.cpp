66c201ef23f11bfe0531454a884232361080ef63
Oblivichrash <Oblivichrash@outlook.com>
2022-06-30 18:16:01 +0800

    feat(pedump): Add import table dump support

--- tools/pedump/main.cpp
+++ tools/pedump/main.cpp
@@ -1,5 +1,7 @@
 // tools/pedump/main.cpp: Dump Binary files
 
+#include <Windows.h>
+
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -88,6 +90,39 @@ std::ostream& memdump(std::ostream& os, void* base, std::size_t rows) {
   return os;
 }
 
+constexpr auto snap_by_ordinal(const IMAGE_THUNK_DATA64& thunk) { return IMAGE_SNAP_BY_ORDINAL64(thunk.u1.Ordinal); }
+constexpr auto snap_by_ordinal(const IMAGE_THUNK_DATA32& thunk) { return IMAGE_SNAP_BY_ORDINAL32(thunk.u1.Ordinal); }
+
+constexpr auto ordinal(const IMAGE_THUNK_DATA64& thunk) { return IMAGE_ORDINAL64(thunk.u1.Ordinal); }
+constexpr auto ordinal(const IMAGE_THUNK_DATA32& thunk) { return IMAGE_ORDINAL32(thunk.u1.Ordinal); }
+
+template <typename THUNK_DATA, typename NT_HEADER>
+std::ostream& import_dump(std::ostream& os, void* base, NT_HEADER& nt) {
+  auto buff = static_cast<char*>(base);
+
+  auto sections = IMAGE_FIRST_SECTION(&nt);
+  for (std::size_t i = 0; i < nt.FileHeader.NumberOfSections; ++i) {
+    auto virtual_address = nt.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+    if (sections[i].VirtualAddress < virtual_address && virtual_address < (sections[i].VirtualAddress + sections[i].Misc.VirtualSize)) {
+      os << sections[i].Name << '\n';
+      auto delta = sections[i].VirtualAddress - sections[i].PointerToRawData;
+
+      for (auto descriptor = reinterpret_cast<IMAGE_IMPORT_DESCRIPTOR*>(&buff[virtual_address - delta]); descriptor->Name; ++descriptor) {
+        std::cout << &buff[descriptor->Name - delta] << '\n';
+
+        for (auto thunk = reinterpret_cast<THUNK_DATA*>(&buff[descriptor->OriginalFirstThunk - delta]); thunk->u1.Ordinal; ++thunk) {
+          if (!snap_by_ordinal(*thunk)) {
+            std::cout << '\t' << &reinterpret_cast<IMAGE_IMPORT_BY_NAME&>(buff[thunk->u1.Function - delta]).Name[0] << '\n';
+          } else {
+            std::cout << '\t' << ordinal(*thunk) << '\n';
+          }
+        }
+      }
+    }
+  }
+  return os;
+}
+
 int main(int argc, char* argv[]) try {
   if (argc < 2) {
     std::cerr << argv[0] << " Version " << BINLAB_VERSION_MAJOR << '.' << BINLAB_VERSION_MINOR << '\n';
@@ -99,8 +134,17 @@ int main(int argc, char* argv[]) try {
     const auto size = is.tellg();
     std::vector<char> buff(size);
     if (is.seekg(0).read(&buff[0], size)) {
-      std::cout << std::hex;
-      memdump(std::cout, buff.data(), 16);
+      auto dos = reinterpret_cast<IMAGE_DOS_HEADER*>(&buff[0]);
+      auto nt = reinterpret_cast<IMAGE_NT_HEADERS64*>(&buff[dos->e_lfanew]);
+      if (nt->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
+        std::cout << "PE64\n";
+        import_dump<IMAGE_THUNK_DATA64>(std::cout, buff.data(),reinterpret_cast<IMAGE_NT_HEADERS64&>(buff[dos->e_lfanew]));
+      } else if (nt->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
+        std::cout << "PE32\n";
+        import_dump<IMAGE_THUNK_DATA32>(std::cout, buff.data(), reinterpret_cast<IMAGE_NT_HEADERS32&>(buff[dos->e_lfanew]));
+      } else {
+        std::cerr << "the optional header magic is invalid\n";
+      }
     } else {
       std::cerr << "read failed\n";
     }
@@ -113,3 +157,4 @@ int main(int argc, char* argv[]) try {
   std::cerr << "error: " << e.what() << '\n';
   return 1;
 }
+
